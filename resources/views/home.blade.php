cc270961f73bc5b0270319f8f520f25454ec1044
Yassine Khachlek <yassine.khachlek@gmail.com>
2018-07-21 14:04:45 +0100

    show users list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-4">
+
+            <div class="list-group">
+            @foreach($users as $user)
+                <a href="#" class="list-group-item list-group-item-action active">
+                    {{ $user->name }}
+                </a>
+            @endforeach
+            </div>
+
+        </div>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
