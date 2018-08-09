967294b0e00f4ab3bfe37ec7559699ba9ff88bc0
Yassine Khachlek <yassine.khachlek@gmail.com>
2018-08-09 16:31:10 +0100

    added docker dev environment

---
+++ docker/compose/dev/up.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $SCRIPT_PATH
+
+LOCAL_PROJECT_PATH="$(dirname $(dirname $(dirname ${SCRIPT_PATH})))"
+
+cd $LOCAL_PROJECT_PATH/docker/compose/dev
+
+docker-compose up -d
